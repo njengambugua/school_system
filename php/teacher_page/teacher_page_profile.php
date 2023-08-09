a71fe05dec7d6877928cac6512cda0e7ee582284
mugoalvin <alvin.mugo@student.moringaschool.com>
2023-08-09 17:11:18 +0300

    Teacher Profile

--- php/teacher_page/teacher_page_profile.php
+++ php/teacher_page/teacher_page_profile.php
@@ -3,9 +3,33 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../css/teacher/profile.css">
   <title>Teacher Portal</title>
 </head>
 <body>
   <?php include('../../php/teacher_navbar.php') ?>
+
+  <main class="main">
+      <div class="main-content-holder">
+        <div>
+          <!-- profile -->
+          <h2>Teacher Profile</h2>
+          <div id="teacherInfo">
+            <div id="information">
+              <span>Name: Mzee Mumunye</span>
+              <span>Employee No:</span>
+              <span>Gender: Male</span>
+              <span>Subjects Taught: Kiswahili, CRE, Science</span>
+              <span>Grade Taught: 4, 5, 6</span>
+              <span>Phone No: 12938138</span>
+              <span>Email: mumunyemzee@gmail.com</span>
+            </div>
+            <div id="teacherPassportDiv">
+              <img src="../../images/yellow_grt.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
+  </main>
 </body>
 </html>
