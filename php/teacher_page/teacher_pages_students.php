344276d91010895775316a712712939948684e92
njengambugua <john898mbugua@gmail.com>
2023-08-09 15:46:56 +0300

    Teacher student rlship

--- php/teacher_page/teacher_pages_students.php
+++ php/teacher_page/teacher_pages_students.php
@@ -4,8 +4,29 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Teacher Portal</title>
+  <link rel="stylesheet" href="../../css/teacher/students.css">
 </head>
 <body>
   <?php include('../../php/teacher_navbar.php') ?>
+  <main class="main">
+    <div class="main-content-holder">
+      <div class="all_students">
+        <h4>Total Students</h4>
+        <h1>180</h1>
+      </div>
+
+      <a href="./teacher_view_student_profile.php"><div class="student_profile">
+        <h4>Students Profile</h4>
+      </div></a>
+
+      <a href="./teacher_view_student_attendance.php"><div class="student_attendance">
+        <h4>Student Attendance</h4>
+      </div></a>
+
+      <a href="./teacher_view_student_academics.php"><div class="student_academics">
+        <h4>Students Academics</h4>
+      </div></a>
+    </div>
+  </main>
 </body>
 </html>
