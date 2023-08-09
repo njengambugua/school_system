501ee182a049fff6654e3dab61eb6981f016bd6e
njengambugua <john898mbugua@gmail.com>
2023-08-09 21:26:06 +0300

    Teacher view student attendance

--- php/teacher_page/teacher_view_student_attendance.php
+++ php/teacher_page/teacher_view_student_attendance.php
@@ -4,8 +4,203 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Teacher Portal</title>
+  <link rel="stylesheet" href="../../css/teacher/view_attendance.css">
 </head>
 <body>
   <?php include('../../php/teacher_navbar.php') ?>
+  <main class="main">
+    <div class="main-content-holder">
+      <form class="select_heads">
+        <div class="left-select-head">
+          <select name="grade" class="select-grade" id="grade">
+            <option value="" selected>Select Grade</option>
+            <option value="">Grade 1</option>
+            <option value="">Grade 1</option>
+            <option value="">Grade 1</option>
+            <option value="">Grade 1</option>
+            <option value="">Grade 1</option>
+          </select>
+        </div>
+        <div class="right-select-head">
+          <select name="grade" class="select-grade" id="grade">
+            <option value="" selected>Select Grade</option>
+            <option value="">Term 1</option>
+            <option value="">Term 2</option>
+            <option value="">Term 3</option>
+          </select>
+        </div>
+        <div class="submit">
+          <button class="select-grade" type="submit">Filter</button>
+        </div>
+      </form>
+      <div class="attendance_desc">
+        <div><h4>Term 1</h4></div>
+        <div><h4>Grade 3</h4></div>
+      </div>
+      <div class="att_rec"><h4>Attendance Records</h4></div>
+      <div class="attendance-tables">
+          <div class="table-holder">
+            <table class="table table-sm` att-table">
+              <thead class="thead-att">
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Mathematics</th>
+                  <th>Mathematics</th>
+                  <th>Mathematics</th>
+                  <th>Mathematics</th>
+                  <th>Mathematics</th>
+                </tr>
+              </thead>
+              <tbody class="tbody-att">
+                <tr>
+                  <td>1</td>
+                  <td>John</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>7</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>8</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>10</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>11</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>12</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>13</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>10</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>Joan</td>
+                  <td>7</td>
+                  <td>6</td>
+                  <td>7</td>
+                  <td>8</td>
+                  <td>9</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+    </div>
+  </main>
 </body>
 </html>
