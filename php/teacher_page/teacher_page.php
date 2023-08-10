5354c2517b4f3fc7c8fe28c51522d37fc5146a18
mugoalvin <alvin.mugo@student.moringaschool.com>
2023-08-10 09:55:55 +0300

    Teacher Home

--- php/teacher_page/teacher_page.php
+++ php/teacher_page/teacher_page.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../css/teacher/students.css">
   <title>Teacher Portal</title>
 </head>
 <body>
@@ -10,6 +11,23 @@
   <main class="main">
     <div class="main-content-holder">
 
+      <a href="./teacher_pages_students.php">
+        <div class="student_profile">
+        <h4>Students Information</h4>
+        </div>
+      </a>
+
+      <a href="./teacher_page_messages.php">
+        <div class="student_profile">
+        <h4>Messages</h4>
+        </div>
+      </a>
+
+      <a href="./teacher_page_profile.php">
+        <div class="student_attendance">
+        <h4>Profile</h4>
+        </div>
+      </a>
     </div>
   </main>
 </body>
