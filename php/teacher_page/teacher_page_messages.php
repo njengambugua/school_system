d196df73bd02eec84407a70683cb8bc29283c327
njengambugua <john898mbugua@gmail.com>
2023-08-10 10:21:19 +0300

    Teacher timetable

--- php/teacher_page/teacher_page_messages.php
+++ php/teacher_page/teacher_page_messages.php
@@ -4,8 +4,34 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Teacher Portal</title>
+  <link rel="stylesheet" href="../../css/teacher/messages.css">
 </head>
 <body>
   <?php include('../../php/teacher_navbar.php') ?>
+  <main class="main">
+      <div class="main-content-holder">
+        <div class="event-container">
+          <div class="event-item">
+            <div class="event-details">
+              <h4>Teachers' Weekly meeting</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing </p>
+            </div>
+            <div class="event-date">
+              <p>20/03/2022</p>
+            </div>
+          </div>
+
+          <div class="event-item">
+            <div class="event-details">
+              <h4>Send your daily report to the admin's email</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing </p>
+            </div>
+            <div class="event-date">
+              <p>20/05/2022</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  </main>
 </body>
 </html>
