f4473c49b0f081e62c99c5c9ec72b3aedd99f2fe
vincent-pc <VincentNdegwa>
2023-08-14 20:20:23 +0300

    made some changes in the applicant module

--- controllers/applicant/applicant_proc.php
+++ controllers/applicant/applicant_proc.php
@@ -1,47 +1,41 @@
 <?php
 include('../../models/applicant/applicant_class.php');
-echo "<hr>";
+
 $obj = (object)$_POST;
-$_SESSION['obj'] = $obj;
-echo "<hr>";
-
 $user = new applicant($obj);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $_SESSION['obj'] = $obj;
 
-if ($_POST['action'] == 'Apply') {
-    echo "<br>Button was clicked";
-    if ($user->create()) {
-        echo "<br>About to include parent";
-        echo "<hr>";
-        include('../../models/parent/parent_class.php');
-        $parent = new parent7($obj);
-        if($parent->create()) {
-            echo "<br>Parent Created";
-        }
-        else {
-            echo "<br>Parent Not Created";
+    if ($_POST['action'] == 'Apply') {
+        echo "<br>Button was clicked";
+        if ($user->create()) {
 
+            include('../../models/parent/parent_class.php');
+            $parent = new parent7($obj);
+            if ($parent->create()) {
+                echo "<br>Parent Created";
+            } else {
+                echo "<br>Parent Not Created";
+            }
+            echo "<hr>";
+            echo "<br>Record Inserted successfully";
+            header('Location: ../../php/examRules.php');
+        } else {
+            echo "<br>Record was not inserted successfully";
+            // header('Location: ../../php/admission.php');
         }
-        echo "<hr>";
-        echo "<br>Record Inserted successfully";
-        header('Location: ../../php/examRules.php');
-    } else {
-        echo "<br>Record was not inserted successfully";
-        // header('Location: ../../php/admission.php');
     }
-} else {
-    echo "<br>Button was not clicked";
-    header('Location: ../../php/admission.php');
 }
 
-echo "<br>Done";
+
 
 if (isset($_GET['applicant'])) {
-    echo "<br>Applicant is set";
-    print_r($_GET);
     $id = $_GET['applicant'];
+    print_r($id);
     $data = $user->retrieve($id);
+    
     if ($data) {
-       $_SESSION['applicant_data'] = $data;
-        // header('Location: ../../php/exams.php');
+        $applicant_level = $data[0]->Level;
+        header("Location: ../exams/exams_proc.php?level=$applicant_level");
     }
 }
