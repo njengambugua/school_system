e7ee6710f3229ddd87806bf21fd12a5df6cebd4b
mugoalvin <alvin.mugo@student.moringaschool.com>
2023-08-18 10:28:56 +0300

    Modified Parent Insertion

--- controllers/applicant/applicant_proc.php
+++ controllers/applicant/applicant_proc.php
@@ -1,43 +1,25 @@
 <?php
 include('../../models/applicant/applicant_class.php');
-echo "<hr>";
+session_start();
 $obj = (object)$_POST;
 $_SESSION['obj'] = $obj;
-echo "<hr>";
 
-$user = new applicant($obj);
+$newApplicant = new applicant($obj);
 
 if ($_POST['action'] == 'Apply') {
-    echo "<br>Button was clicked";
-    if ($user->create()) {
-        echo "<br>About to include parent";
-        echo "<hr>";
-        include('../../models/parent/parent_class.php');
-        $parent = new parent7($obj);
-        if($parent->create()) {
-            echo "<br>Parent Created";
-        }
-        else {
-            echo "<br>Parent Not Created";
-
-        }
-        echo "<hr>";
-        echo "<br>Record Inserted successfully";
-        header('Location: ../../php/examRules.php');
+    if ($newApplicant->create()) {
+        header('Location: ../parent/parent_proc.php');
     } else {
-        echo "<br>Record was not inserted successfully";
-        // header('Location: ../../php/admission.php');
+        header('Location: ../../php/admission.php');
     }
-} else {
-    echo "<br>Button was not clicked";
+}
+else {
     header('Location: ../../php/admission.php');
 }
 
-echo "<br>Done";
-
 if (isset($_GET['applicant'])) {
     echo "<br>Applicant is set";
-    print_r($_GET);
+    // print_r($_GET);
     $id = $_GET['applicant'];
     $data = $user->retrieve($id);
     if ($data) {
