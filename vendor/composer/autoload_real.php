7b7bc63ae319475955537a6514ad02fe8487991e
vincent-pc <VincentNdegwa>
2023-08-21 13:16:02 +0300

    added some sms packages

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInite6b3e6c2e3639abae1af9744627b030f
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInite6b3e6c2e3639abae1af9744627b030f::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
