6f80e85d16064f4904ab6e489857f0943187b405
njengambugua <john898mbugua@gmail.com>
2023-08-23 20:43:29 +0300

    teacher_level relationship

---
+++ models/teacher_level/teacher_level_DBO.php
@@ -0,0 +1,35 @@
+<?php
+include('../../DB.php');
+session_start();
+
+class Teacher_level_DBO
+{
+  public $conn;
+  public $sql;
+  public $stmt;
+  public $lastInsertId;
+  public $res;
+  public $numRows;
+  public $error;
+
+  public function __construct()
+  {
+    $db = new DatabaseConnection;
+    $this->conn = $db->getConnection();
+  }
+
+  public function insert($obj) {
+    try {
+      $this->sql = "INSERT INTO teacher_level(teacher_id,level_id)VALUES(:teacher_id,:level_id)";
+      $this->stmt = $this->conn->prepare($this->sql);
+      $this->stmt->bindParam(':teacher_id',$obj->teacher_id);
+      $this->stmt->bindParam(':level_id',$obj->level_id);
+      $this->stmt->execute();
+      $this->lastInsertId = $this->conn->lastInsertId();
+      return true;
+    } catch (PDOException $e) {
+      $this->error = $e->getMessage();
+      return false;
+    }
+  }
+}
